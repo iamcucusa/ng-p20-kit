cbf87ab44c6ae50f988094dd5618732d6e5e4c36
Grace Henriquez <graciela.fernandez@businesspartner.roche.com>
2025-10-14 14:57:31 +0200

    Replace custom buttons with PrimeNG components including pRipple effects

--- projects/showcase/src/app/trial/trials-container.component.ts
+++ projects/showcase/src/app/trial/trials-container.component.ts
@@ -1,17 +1,15 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { PageHeadingComponent } from '@core/page-heading.component';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'pg-trials-container',
   templateUrl: './trials-container.component.html',
   styleUrls: ['./trials-container.component.scss'],
   standalone: true,
-  imports: [PageHeadingComponent]
+  imports: [PageHeadingComponent, ButtonModule, RippleModule]
 })
 export class TrialsContainerComponent {
-  @ViewChild('pageTitleTemplate') pageTitleTemplate!: TemplateRef<any>;
-  @ViewChild('pageDescriptionTemplate') pageDescriptionTemplate!: TemplateRef<any>;
-  @ViewChild('pageActionsTemplate') pageActionsTemplate!: TemplateRef<any>;
-
   constructor() { }
 }
